1901b0ee75a7229e8f1128b0f460b3aff6ea2aea
Miguel Jimenez <denkatalabeno@gmail.com>
2021-11-17 21:52:36 +0800

    yay! we
    are rendering on vue!

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SegmentedControl v-bind:filters="['price', 'relevance', 'distance']"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SegmentedControl from './components/SegmentedControl.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SegmentedControl
   }
 }
 </script>
@@ -19,7 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/SegmentedControl.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="root">
+    <h2> This is segmented Control! </h2>
+      <p :key="item" v-for="item in searchFilters">
+        {{ item }}
+      </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SegmentedControl',
+  props: {
+    filters: Array
+  },
+  data: function() {
+    return { searchFilters: this.filters };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
