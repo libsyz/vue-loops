6dead2b702fc1d7306c5f91d55ef6795294eda1a
Miguel Jimenez <denkatalabeno@gmail.com>
2021-12-21 23:41:53 +0800

    pseudocoded the approach for building the typer

--- src/components/TypingCursor.vue
+++ src/components/TypingCursor.vue
@@ -1,9 +1,30 @@
 <template>
-  <h3> We can help you with testing </h3>
+  <h3> We can help you with <span> {{ currentWord }} </span> </h3>
 </template>
 
 <script>
 export default {
+  mounted() {
+    ['testing', 'development'].forEach((word) => {
+      let counter = 0;
+      let int = setInterval(() => {
+          if (counter === word.length) {
+            clearInterval(int);
+          } else {
+            this.currentWord = word.substring(0, counter);
+          }
+      }, 2000);
+    })
+  },
+  data: function() {
+    return {
+      currentWord: 'testing',
+      words: ['testing', 'development', 'production']
+    }
+  },
+  methods: {
+
+  }
 
 }
 </script>
